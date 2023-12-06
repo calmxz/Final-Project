68f87b58e5489ec471adacf578e63a90b6daded7
calmxz <128876131+calmxz@users.noreply.github.com>
2023-12-06 21:11:08 +0800

    Update App.php

---
+++ SnackCraving/src/App.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$database = "";
+$username = "root";
+$password = "1234";
+
+/**
+ * TODO: DATABASE CONNECTION
+ * *use infoman midterm as basis
+ * ! DONT FORGET GRRR
+ */
+try {
+
+    
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
