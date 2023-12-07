a6ee39abac421cf113a6dca5d9271395b701a92e
calmxz <128876131+calmxz@users.noreply.github.com>
2023-12-07 15:57:54 +0800

    Update app.js

--- SnackCraving/web/app.js
+++ SnackCraving/web/app.js
@@ -3,6 +3,7 @@
 function loadHome(page) {
     // Set the active class for the home navbar item
     document.getElementById('home-nav').classList.add('active', 'bg-gray-950');
+    document.getElementById('menu-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('customer-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('transaction-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('admin-nav').classList.remove('active', 'bg-gray-950');
@@ -20,6 +21,7 @@ function loadCustomer(page) {
     // Set the active class for the customer navbar item
     document.getElementById('customer-nav').classList.add('active', 'bg-gray-950');
     document.getElementById('home-nav').classList.remove('active', 'bg-gray-950');
+    document.getElementById('menu-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('transaction-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('admin-nav').classList.remove('active', 'bg-gray-950');
 
@@ -35,6 +37,7 @@ function loadCustomer(page) {
 function loadTransaction(page){
     // Set the active class for the transaction navbar item
     document.getElementById('transaction-nav').classList.add('active', 'bg-gray-950');
+    document.getElementById('menu-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('customer-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('home-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('admin-nav').classList.remove('active', 'bg-gray-950');
@@ -51,6 +54,7 @@ function loadTransaction(page){
 function loadAdmin(page){
     // Set the active class for the admin navbar item
     document.getElementById('admin-nav').classList.add('active', 'bg-gray-950');
+    document.getElementById('menu-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('customer-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('transaction-nav').classList.remove('active', 'bg-gray-950');
     document.getElementById('home-nav').classList.remove('active', 'bg-gray-950');
