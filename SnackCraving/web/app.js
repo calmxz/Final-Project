07288602f834b782f981356b369b1aa2c9957f1b
calmxz <alverdeedwardandrew@gmail.com>
2023-12-11 14:25:42 +0800

    created menu page with placeholder text, tried icons8

--- SnackCraving/web/app.js
+++ SnackCraving/web/app.js
@@ -17,6 +17,22 @@ function loadHome(page) {
         .catch(error => console.error('Error:', error));
 }
 
+function loadMenu(page){
+    document.getElementById('menu-nav').classList.add('active', 'bg-gray-950');
+    document.getElementById('home-nav').classList.remove('active', 'bg-gray-950');
+    document.getElementById('customer-nav').classList.remove('active', 'bg-gray-950');
+    document.getElementById('transaction-nav').classList.remove('active', 'bg-gray-950');
+    document.getElementById('admin-nav').classList.remove('active', 'bg-gray-950');   
+
+    fetch(page)
+        .then (response => response.text())
+        .then (data => {
+            const content = new DOMParser().parseFromString(data, 'text/html').querySelector('#content').innerHTML;
+            document.getElementById('content').innerHTML = content;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
 function loadCustomer(page) {
     // Set the active class for the customer navbar item
     document.getElementById('customer-nav').classList.add('active', 'bg-gray-950');
--- SnackCraving/web/home.php
+++ SnackCraving/web/home.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="app.js"></script>
 </head>
+
 <body class="flex flex-col md:flex-row h-screen bg-gray-50">
     <!-- Side Navigation Bar -->
     <nav class="bg-gray-800 text-white w-full md:w-64 md:h-screen overflow-y-auto">
@@ -16,13 +18,13 @@
         </div>
         <ul>
             <li id="home-nav" class="p-4 transition duration-300 ease-in-out hover:bg-gray-900 active: bg-gray-950">
-                <a href="#" onclick="loadHome('home.html')" class="flex items-center hover:cursor-pointer">
+                <a href="#" onclick="loadHome('home.php')" class="flex items-center hover:cursor-pointer">
                     <i class="fas fa-home mr-2"></i> Home
                 </a>
             </li>
-            <li id="home-nav" class="p-4 transition duration-300 ease-in-out hover:bg-gray-90">
-                <a href="#" onclick="loadHome('home.html')" class="flex items-center hover:cursor-pointer">
-                    <i class="fa-solid fa-burger-fries"></i> Menu
+            <li id="menu-nav" class="p-4 transition duration-300 ease-in-out hover:bg-gray-900">
+                <a href="#" onclick="loadMenu('menu.html')" class="flex items-center hover:cursor-pointer">
+                    <img src="src/icons/menu.png" alt="Menu icon" class="w-6 h-6 mr-2" /> Menu
                 </a>
             </li>
             <li id="customer-nav" class="p-4 transition duration-300 ease-in-out hover:bg-gray-900">
@@ -45,35 +47,36 @@
 
     <!-- Main Content -->
     <!-- Dashboard -->
-<div id="content" class="flex-1 p-8">
-    <h1 class="text-3xl font-semibold">HOME/DASHBOARD</h1>
+    <div id="content" class="flex-1 p-8">
+        <h1 class="text-3xl font-semibold">HOME/DASHBOARD</h1>
 
-    <div>
-    <table class="w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl border-collapse m-auto border border-gray-200 p-5">
-            <thead class="bg-gray-50 border-b-2 border-gray-300">
-                <tr>
-                    <th class="p-3 font-semibold text-left">ID</th>
-                    <th class="p-3 font-semibold text-left">Category</th>
-                </tr>
-            </thead>
+        <div>
+            <table class="w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl border-collapse m-auto border border-gray-200 p-5">
+                <thead class="bg-gray-50 border-b-2 border-gray-300">
+                    <tr>
+                        <th class="p-3 font-semibold text-left">ID</th>
+                        <th class="p-3 font-semibold text-left">Category</th>
+                    </tr>
+                </thead>
 
-            <tbody class ="divide-y divide-gray-100">
-                <?php
-                include 'src\fetch_data.php';
+                <tbody class="divide-y divide-gray-100">
+                    <?php
+                    include 'src\fetch_data.php';
 
-                foreach ($data as $index => $row) {
-                    $rowClass = $index % 2 === 0 ? 'bg-gray-200' : '';
-                    echo "<tr class='$rowClass'>";
-                    echo "<td class='p-3 text-left text-gray-700 whitespace-nowrap'>" . $row['product_category_id'] . "</td>";
-                    echo "<td class='p-3 text-left text-gray-700'>" . $row['product_category'] . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
+                    foreach ($data as $index => $row) {
+                        $rowClass = $index % 2 === 0 ? 'bg-gray-200' : '';
+                        echo "<tr class='$rowClass'>";
+                        echo "<td class='p-3 text-left text-gray-700 whitespace-nowrap'>" . $row['product_category_id'] . "</td>";
+                        echo "<td class='p-3 text-left text-gray-700'>" . $row['product_category'] . "</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
 
 
 </body>
+
 </html>
